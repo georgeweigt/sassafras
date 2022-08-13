bc0ed860178a62ef3414b10141b6c592b25817e7
georgeweigt <gweigt@icloud.com>
2022-08-13 07:51:06 -0700

    update

--- src/parse_comment_stmt.c
+++ src/parse_comment_stmt.c
@@ -3,15 +3,7 @@
 void
 parse_comment_stmt(void)
 {
-	while (*inp) {
-
-		// semicolon or end of line terminates comment statement
-
-		if (*inp == ';' || *inp == '\n' || *inp == '\r')
-			break;
-
+	while (*inp && *inp != ';')
 		inp++;
-	}
-
 	scan();
 }
