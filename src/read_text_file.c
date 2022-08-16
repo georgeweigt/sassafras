c698faf4f4a7b759ea9208de92e1ee64c830eba6
georgeweigt <gweigt@icloud.com>
2022-08-16 06:35:25 -0700

    Update read_text_file.c

--- src/read_text_file.c
+++ src/read_text_file.c
@@ -9,37 +9,28 @@ read_text_file(char *filename)
 	int fd, n;
 	char *buf;
 
-	fd = open(filename, O_RDONLY, 0);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1) {
 		fprintf(stderr, "cannot open %s\n", filename);
-		return NULL;
+		exit(1);
 	}
 
 	n = lseek(fd, 0, SEEK_END);
 
-	if (n < 0) {
-		close(fd);
-		return NULL;
-	}
+	if (n < 0)
+		exit(1);
 
-	if (lseek(fd, 0, SEEK_SET)) {
-		close(fd);
-		return NULL;
-	}
+	if (lseek(fd, 0, SEEK_SET))
+		exit(1);
 
 	buf = malloc(n + 1);
 
-	if (buf == NULL) {
-		close(fd);
-		return NULL;
-	}
+	if (buf == NULL)
+		exit(1);
 
-	if (read(fd, buf, n) != n) {
-		close(fd);
-		free(buf);
-		return NULL;
-	}
+	if (read(fd, buf, n) != n)
+		exit(1);
 
 	close(fd);
 
