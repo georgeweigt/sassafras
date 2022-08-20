5b8554f9ec0c0633294625df147cbe3c3cb0cc66
georgeweigt <gweigt@icloud.com>
2022-08-20 09:01:52 -0700

    update

--- src/tdist.c
+++ src/tdist.c
@@ -7,13 +7,11 @@
 double
 tdist(double t, double df)
 {
-	double a;
-	if (isnan(t) || isnan(df) || df < 1.0)
+	double x;
+	if (!isfinite(t) || !isfinite(df) || df < 1.0)
 		return NAN;
-	a = 0.5 * betai(0.5 * df, 0.5, df / (df + t * t));
-	if (t > 0.0)
-		a = 1.0 - a;
-	return a;
+	x = (t + sqrt(t * t + df)) / (2.0 * sqrt(t * t + df));
+	return incbeta(df / 2.0, df / 2.0, x);
 }
 
 // t-distribution quantile function, like qt() in R
@@ -25,7 +23,7 @@ qt(double p, double df)
 {
 	int i;
 	double a, t, t1, t2;
-	if (isnan(p) || isnan(df) || df < 1.0)
+	if (!isfinite(p) || !isfinite(df) || df < 1.0)
 		return NAN;
 	t1 = -1000.0;
 	t2 = 1000.0;
@@ -47,83 +45,9 @@ qt(double p, double df)
 double
 fdist(double t, double df1, double df2)
 {
-	if (isnan(t) || isnan(df1) || isnan(df2) || df1 < 1.0 || df2 < 1.0)
+	if (!isfinite(t) || !isfinite(df1) || !isfinite(df2) || df1 < 1.0 || df2 < 1.0)
 		return NAN;
 	if (t < 0.0)
 		return 0.0;
-	else
-		return betai(0.5 * df1, 0.5 * df2, t / (t + df2 / df1));
-}
-
-// From Numerical Recipes in C, p. 214
-
-double
-gammln(double xx)
-{
-	double x,y,tmp,ser;
-	static double cof[6]={76.18009172947146,-86.50532032941677,
-		24.01409824083091,-1.231739572450155,
-		0.1208650973866179e-2,-0.5395239384953e-5};
-	int j;
-	y=x=xx;
-	tmp=x+5.5;
-	tmp -= (x+0.5)*log(tmp);
-	ser=1.000000000190015;
-	for (j=0;j<=5;j++) ser += cof[j]/++y;
-	return -tmp+log(2.5066282746310005*ser/x);
-}
-
-// From Numerical Recipes in C, p. 227
-
-double
-betai(double a, double b, double x)
-{
-	double bt;
-	if (x < 0.0 || x > 1.0) return NAN;
-	if (x == 0.0 || x == 1.0) bt=0.0;
-	else
-		bt=exp(gammln(a+b)-gammln(a)-gammln(b)+a*log(x)+b*log(1.0-x));
-	if (x < (a+1.0)/(a+b+2.0))
-		return bt*betacf(a,b,x)/a;
-	else
-		return 1.0-bt*betacf(b,a,1.0-x)/b;
-}
-
-// From Numerical Recipes in C, p. 227
-
-#define FPMIN 1.0e-30
-
-double
-betacf(double a, double b, double x)
-{
-	int m,m2;
-	double aa,c,d,del,h,qab,qam,qap;
-	qab=a+b;
-	qap=a+1.0;
-	qam=a-1.0;
-	c=1.0;
-	d=1.0-qab*x/qap;
-	if (fabs(d) < FPMIN) d=FPMIN;
-	d=1.0/d;
-	h=d;
-	for (m=1;m<=100;m++) {
-		m2=2*m;
-		aa=m*(b-m)*x/((qam+m2)*(a+m2));
-		d=1.0+aa*d;
-		if (fabs(d) < FPMIN) d=FPMIN;
-		c=1.0+aa/c;
-		if (fabs(c) < FPMIN) c=FPMIN;
-		d=1.0/d;
-		h *= d*c;
-		aa = -(a+m)*(qab+m)*x/((a+m2)*(qap+m2));
-		d=1.0+aa*d;
-		if (fabs(d) < FPMIN) d=FPMIN;
-		c=1.0+aa/c;
-		if (fabs(c) < FPMIN) c=FPMIN;
-		d=1.0/d;
-		del=d*c;
-		h *= del;
-		if (fabs(del-1.0) < 1e-10) break;
-	}
-	return h;
+	return incbeta(df1 / 2.0, df2 / 2.0, t / (t + df2 / df1));
 }
