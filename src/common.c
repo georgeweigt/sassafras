e883c64886c749c8566d2a2126b27a465b585ca6
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-01-13 13:21:17 -0700

    update

--- src/common.c
+++ src/common.c
@@ -71,3 +71,15 @@ int xx[MAXVAR];
 int ytab[MAXVAR];
 int yvar;
 struct dataset *dataset;
+
+char *fmt[9] = {
+	"%0.0f",
+	"%0.1f",
+	"%0.2f",
+	"%0.3f",
+	"%0.4f",
+	"%0.5f",
+	"%0.6f",
+	"%0.7f",
+	"%0.8f",
+};
